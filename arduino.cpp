69e3c03b9257528d73ac319b496eb0d8144df579
Augastin K Lazar <laugastin@gmail.com>
2023-04-29 18:47:21 +0530

    Create arduino.cpp

---
+++ arduino.cpp
@@ -0,0 +1,124 @@
+#include <Wire.h>
+#include <math.h>
+
+// Constants
+#define GYRO_SCALE_FACTOR 1000.0f // Gyroscope scale factor in degrees per second
+#define ACCEL_SCALE_FACTOR 100.0f // Accelerometer scale factor in meters per second^2
+#define KP 1.0f // Proportional gain
+#define KI 0.0f // Integral gain
+#define KD 0.0f // Derivative gain
+
+// Global variables
+float gyro_x, gyro_y, gyro_z; // Gyroscope readings
+float accel_x, accel_y, accel_z; // Accelerometer readings
+float angle_x, angle_y; // Angle readings
+float error_x, error_y; // Error readings
+float integral_x, integral_y; // Integral readings
+float derivative_x, derivative_y; // Derivative readings
+float output_x, output_y; // Output readings
+
+// Functions
+void read_sensors() {
+  // Read the gyroscope and accelerometer readings
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B);
+  Wire.endTransmission();
+  Wire.requestFrom(0x68, 6);
+  while (Wire.available()) {
+    gyro_x = Wire.read() << 8 | Wire.read();
+    gyro_y = Wire.read() << 8 | Wire.read();
+    gyro_z = Wire.read() << 8 | Wire.read();
+  }
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3C);
+  Wire.endTransmission();
+  Wire.requestFrom(0x68, 6);
+  while (Wire.available()) {
+    accel_x = Wire.read() << 8 | Wire.read();
+    accel_y = Wire.read() << 8 | Wire.read();
+    accel_z = Wire.read() << 8 | Wire.read();
+  }
+}
+
+void calculate_angles() {
+  // Calculate the angle readings from the gyroscope and accelerometer readings
+  angle_x = (gyro_x * GYRO_SCALE_FACTOR) * (float)millis() / 1000.0f;
+  angle_y = (gyro_y * GYRO_SCALE_FACTOR) * (float)millis() / 1000.0f;
+  angle_z = (accel_z * ACCEL_SCALE_FACTOR) / 9.81f;
+}
+
+void calculate_errors() {
+  // Calculate the error readings from the angle readings
+  error_x = angle_x - previous_angle_x;
+  error_y = angle_y - previous_angle_y;
+}
+
+void calculate_integrals() {
+  // Calculate the integral readings from the error readings
+  integral_x += error_x * (float)millis() / 1000.0f;
+  integral_y += error_y * (float)millis() / 1000.0f;
+}
+
+void calculate_derivatives() {
+  // Calculate the derivative readings from the error readings
+  derivative_x = (error_x - previous_error_x) / (float)millis() / 1000.0f;
+  derivative_y = (error_y - previous_error_y) / (float)millis() / 1000.0f;
+}
+
+void calculate_outputs() {
+  // Calculate the output readings from the proportional, integral, and derivative readings
+  output_x = KP * error_x + KI * integral_x + KD * derivative_x;
+  output_y = KP * error_y + KI * integral_y + KD * derivative_y;
+}
+
+void update_servos() {
+  // Update the servo positions based on the output readings
+  analogWrite(SERVO_X_PIN, output_x);
+  analogWrite(SERVO_Y_PIN, output_y);
+}
+
+void setup() {
+  // Initialize the system
+  Serial.begin(9600);
+  Wire.begin();
+  // Initialize the IMU sensor
+  IMU_Init();
+
+  // Set the initial angle readings to zero
+  angle_x = 0.0f;
+  angle_y = 0.0f;
+
+  // Start the PID controller
+  PID_Controller_Start();
+}
+
+// Main loop
+void loop() {
+  // Read the sensor readings
+  read_sensors();
+
+  // Calculate the angles
+  calculate_angles();
+
+  // Calculate the errors
+  calculate_errors();
+
+  // Calculate the integrals
+  calculate_integrals();
+
+  // Calculate the derivatives
+  calculate_derivatives();
+
+  // Calculate the outputs
+  calculate_outputs();
+
+  // Update the servo positions
+  update_servos();
+
+  // Print the angle readings to the serial monitor
+  Serial.print("Angle X: ");
+  Serial.print(angle_x);
+  Serial.print("   Angle Y: ");
+  Serial.println(angle_y);
+}
+
